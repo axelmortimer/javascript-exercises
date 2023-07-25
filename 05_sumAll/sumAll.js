0ddf423fea65c6738601cba2103aa28c5694a0a6
axelmortimer <6845370+axelmortimer@users.noreply.github.com>
2023-07-25 18:08:46 -0400

    Completed 05 exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+   let sum = 0;
+   if ((typeof a == 'number' && a >= 0) && (typeof b == 'number' && b >= 0)) {
+      let first = a < b ? a : b;
+      let last = a < b ? b : a;
+      for (let i = first; i <= last; i++) {
+         sum += i;
+      }
+   } else {
+      return 'ERROR'
+   }
+   return sum;
 };
 
 // Do not edit below this line
