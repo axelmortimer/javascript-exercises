66a3e644fa898122e67841b04fb4ad706811b8dc
axelmortimer <6845370+axelmortimer@users.noreply.github.com>
2023-07-25 17:09:41 -0400

    Completed 02 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeat) {
+   if (repeat >= 0) {
+      let newString = '';
+      for (let i = 0; i < repeat; i++) {
+         newString = newString + string;
+      }
+      return newString;
+   } else {
+      return 'ERROR';
+   }
 };
 
 // Do not edit below this line
