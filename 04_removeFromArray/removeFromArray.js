02acee1bf3f41bb1475054ca3731cda724384e8f
axelmortimer <6845370+axelmortimer@users.noreply.github.com>
2023-07-25 17:46:39 -0400

    Completed 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(userArray) {
+   let newArray = userArray;
+   for (let i = 0; i < arguments.length; i++) {
+      newArray = newArray.filter(word => word !== arguments[i]);
+   }
+   return newArray;
 };
 
 // Do not edit below this line
