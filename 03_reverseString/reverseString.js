c0a04aaf26c3a322fb0232080b3f80c000ec0e5b
axelmortimer <6845370+axelmortimer@users.noreply.github.com>
2023-07-25 17:17:50 -0400

    Completed 03 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+   let newString = '';
+   for (let i = 1; i <= string.length; i++) {
+      let letter = string.charAt(string.length - i);
+      newString += letter;
+   }
+   return newString;
 };
 
 // Do not edit below this line
